9b73c71ec098d5fe08475f70eb50ef1594cdb205
Andreas Fagschlunger <andreas.fagschlunger@reflex.at>
2012-07-21 20:45:30 +0200

    created

---
+++ at.o2xfs.operator/src/at/o2xfs/operator/task/xfs/ptr/PTRCapabilitiesTask.java
@@ -0,0 +1,110 @@
+/*
+ * Copyright (c) 2012, Andreas Fagschlunger. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   - Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *   - Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package at.o2xfs.operator.task.xfs.ptr;
+
+import at.o2xfs.log.Logger;
+import at.o2xfs.log.LoggerFactory;
+import at.o2xfs.operator.task.ExecuteTaskCommand;
+import at.o2xfs.operator.task.Task;
+import at.o2xfs.operator.ui.content.table.Table;
+import at.o2xfs.operator.ui.content.text.Label;
+import at.o2xfs.xfs.XfsException;
+import at.o2xfs.xfs.ptr.WFSPTRCAPS;
+import at.o2xfs.xfs.service.ptr.PTRCapabilitiesCallable;
+import at.o2xfs.xfs.service.ptr.PTRService;
+import at.o2xfs.xfs.service.util.ExceptionUtil;
+
+public class PTRCapabilitiesTask extends Task {
+
+	private static final Logger LOG = LoggerFactory
+			.getLogger(PTRCapabilitiesTask.class);
+
+	private final PTRService ptrService;
+
+	private Table table = null;
+
+	public PTRCapabilitiesTask(final PTRService ptrService) {
+		if (ptrService == null) {
+			ExceptionUtil.nullArgument("ptrService");
+		}
+		this.ptrService = ptrService;
+	}
+
+	@Override
+	protected void execute() throws Exception {
+		final String method = "execute()";
+		try {
+			final WFSPTRCAPS caps = new PTRCapabilitiesCallable(ptrService)
+					.call();
+			if (LOG.isDebugEnabled()) {
+				LOG.debug(method, "caps=" + caps);
+			}
+			table = new Table(getClass(), "Capability", "Value");
+			addRow("ServiceClass", caps.getServiceClass());
+			addRow("Type", caps.getType());
+			addRow("Compound", caps.isCompound());
+			addRow("Resolution", caps.getResolution());
+			addRow("ReadForm", caps.getReadForm());
+			addRow("WriteForm", caps.getWriteForm());
+			addRow("Extents", caps.getExtents());
+			addRow("Control", caps.getControl());
+			addRow("MaxMediaOnStacker", caps.getMaxMediaOnStacker());
+			addRow("AcceptMedia", caps.isAcceptMedia());
+			addRow("MultiPage", caps.isMultiPage());
+			addRow("PaperSources", caps.getPaperSources());
+			addRow("MediaTaken", caps.isMediaTaken());
+			addRow("MaxRetracts", caps.getMaxRetracts());
+			addRow("ImageType", caps.getImageType());
+			addRow("FrontImageColorFormat", caps.getFrontImageColorFormat());
+			addRow("BackImageColorFormat", caps.getBackImageColorFormat());
+			addRow("CodelineFormat", caps.getCodelineFormat());
+			addRow("ImageSource", caps.getImageSource());
+			addRow("CharSupport", caps.getCharSupport());
+			addRow("DispensePaper", caps.isDispensePaper());
+			addRow("Extra", caps.getExtra());
+			addRow("GuidLights", caps.getGuidLights());
+			addRow("WindowsPrinter", caps.getWindowsPrinter());
+			addRow("MediaPresented", caps.isMediaPresented());
+			addRow("AutoRetractPeriod", caps.getAutoRetractPeriod());
+			addRow("RetractToTransport", caps.isRetractToTransport());
+			addRow("PowerSaveControl", caps.isPowerSaveControl());
+			addRow("CoercivityType", caps.getCoercivityType());
+			addRow("ControlPassbook", caps.getControlPassbook());
+			taskManager.setContent(table);
+		} catch (final XfsException e) {
+			showError(e);
+		}
+		taskManager.setBackCommand(new ExecuteTaskCommand(getParent(),
+				taskManager));
+	}
+
+	private void addRow(final String label, final Object value) {
+		table.addRow(new Label(getClass(), label), value);
+	}
+
+}
---
+++ at.o2xfs.operator/src/at/o2xfs/operator/task/xfs/ptr/PTRStatusTask.java
@@ -0,0 +1,95 @@
+/*
+ * Copyright (c) 2012, Andreas Fagschlunger. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   - Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *   - Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package at.o2xfs.operator.task.xfs.ptr;
+
+import at.o2xfs.operator.task.ExecuteTaskCommand;
+import at.o2xfs.operator.task.Task;
+import at.o2xfs.operator.ui.content.table.Table;
+import at.o2xfs.operator.ui.content.text.Label;
+import at.o2xfs.xfs.XfsException;
+import at.o2xfs.xfs.ptr.PTRPaper;
+import at.o2xfs.xfs.ptr.WFSPTRSTATUS;
+import at.o2xfs.xfs.service.ptr.PTRService;
+import at.o2xfs.xfs.service.ptr.PTRStatusCallable;
+import at.o2xfs.xfs.service.util.ExceptionUtil;
+
+public class PTRStatusTask extends Task {
+
+	private final PTRService ptrService;
+
+	private Table table = null;
+
+	protected PTRStatusTask(final PTRService ptrService) {
+		if (ptrService == null) {
+			ExceptionUtil.nullArgument("ptrService");
+		}
+		this.ptrService = ptrService;
+	}
+
+	@Override
+	protected void execute() throws Exception {
+		table = new Table(getClass(), "Component", "Value");
+		try {
+			final WFSPTRSTATUS status = new PTRStatusCallable(ptrService)
+					.call();
+			addRow("Device", status.getDevice());
+			addRow("Media", status.getMedia());
+			addPaperSupplyStates(status.getPaper());
+			addRow("Toner", status.getToner());
+			addRow("Ink", status.getInk());
+			addRow("Lamp", status.getLamp());
+			addRow("RetractBins", status.getRetractBins());
+			addRow("MediaOnStacker", status.getMediaOnStacker());
+			addRow("Extra", status.getExtra());
+			addRow("GuidLights", status.getGuidLights());
+			addRow("DevicePosition", status.getDevicePosition());
+			addRow("PowerSaveRecoveryTime", status.getPowerSaveRecoveryTime());
+			addRow("PaperType", status.getPaperType());
+			addRow("AntiFraudModule", status.getAntiFraudModule());
+			taskManager.setContent(table);
+		} catch (final XfsException e) {
+			showError(e);
+		}
+		taskManager.setBackCommand(new ExecuteTaskCommand(getParent(),
+				taskManager));
+	}
+
+	private void addPaperSupplyStates(final PTRPaper[] paperStates) {
+		for (int i = 0; i < paperStates.length; i++) {
+			if (PTRPaper.NOTSUPP.equals(paperStates[i])) {
+				continue;
+			}
+			addRow("Paper_" + i, paperStates[i]);
+		}
+	}
+
+	private void addRow(final String label, final Object value) {
+		table.addRow(new Label(getClass(), label), value);
+	}
+
+}
