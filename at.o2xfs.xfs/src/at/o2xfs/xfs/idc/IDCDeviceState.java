8db215be31c610bca7e4186fb7d005907228d7d0
Andreas Fagschlunger <andreas.fagschlunger@reflex.at>
2012-07-21 19:36:17 +0200

    removed prefix: WFS_IDC_

--- at.o2xfs.xfs/src/at/o2xfs/xfs/idc/IDCDeviceState.java
+++ at.o2xfs.xfs/src/at/o2xfs/xfs/idc/IDCDeviceState.java
@@ -27,14 +27,6 @@
 
 package at.o2xfs.xfs.idc;
 
-import static at.o2xfs.xfs.XfsDeviceState.WFS_STAT_DEVBUSY;
-import static at.o2xfs.xfs.XfsDeviceState.WFS_STAT_DEVFRAUDATTEMPT;
-import static at.o2xfs.xfs.XfsDeviceState.WFS_STAT_DEVHWERROR;
-import static at.o2xfs.xfs.XfsDeviceState.WFS_STAT_DEVNODEVICE;
-import static at.o2xfs.xfs.XfsDeviceState.WFS_STAT_DEVOFFLINE;
-import static at.o2xfs.xfs.XfsDeviceState.WFS_STAT_DEVONLINE;
-import static at.o2xfs.xfs.XfsDeviceState.WFS_STAT_DEVPOWEROFF;
-import static at.o2xfs.xfs.XfsDeviceState.WFS_STAT_DEVUSERERROR;
 import at.o2xfs.xfs.XfsConstant;
 import at.o2xfs.xfs.XfsDeviceState;
 
@@ -44,51 +36,55 @@ public enum IDCDeviceState implements XfsConstant {
 	 * The device is present, powered on and online (i.e., operational, not busy
 	 * processing a request and not in an error state).
 	 */
-	WFS_IDC_DEVONLINE(WFS_STAT_DEVONLINE),
+	ONLINE(XfsDeviceState.ONLINE),
 
 	/**
 	 * The device is present and powered on, but offline (not operational�e.g.,
 	 * an operator has switched it offline).
 	 */
-	WFS_IDC_DEVOFFLINE(WFS_STAT_DEVOFFLINE),
+	OFFLINE(XfsDeviceState.OFFLINE),
 
 	/**
 	 * The device is present but powered off.
 	 */
-	WFS_IDC_DEVPOWEROFF(WFS_STAT_DEVPOWEROFF),
+	POWEROFF(XfsDeviceState.POWEROFF),
 
 	/**
 	 * The device is present and is busy processing an Execute request.
 	 */
-	WFS_IDC_DEVBUSY(WFS_STAT_DEVBUSY),
+	BUSY(XfsDeviceState.BUSY),
 
 	/**
 	 * There is no device connected.
 	 */
-	WFS_IDC_DEVNODEVICE(WFS_STAT_DEVNODEVICE),
+	NODEVICE(XfsDeviceState.NODEVICE),
 
 	/**
 	 * The device is present but inoperable due to a hardware fault that
 	 * prevents it from being used.
 	 */
-	WFS_IDC_DEVHWERROR(WFS_STAT_DEVHWERROR),
+	HWERROR(XfsDeviceState.HWERROR),
 
 	/**
 	 * The device is present but a person is preventing proper device operation.
 	 * The application should suspend the device operation or remove the device
 	 * from service until the service provider generates a device state change
 	 * event indicating the condition of the device has changed e.g.the error is
-	 * removed (WFS_IDC_DEVONLINE) or a permanent error condition has occurred
-	 * (WFS_IDC_DEVHWERROR).
+	 * removed (ONLINE) or a permanent error condition has occurred (HWERROR).
 	 */
-	WFS_IDC_DEVUSERERROR(WFS_STAT_DEVUSERERROR),
+	USERERROR(XfsDeviceState.USERERROR),
 
 	/**
 	 * The device is present but has detected a fraud attempt.
 	 * 
 	 * @since 3.10
 	 */
-	WFS_IDC_DEVFRAUDATTEMPT(WFS_STAT_DEVFRAUDATTEMPT);
+	FRAUDATTEMPT(XfsDeviceState.FRAUDATTEMPT),
+
+	/**
+	 * @since 3.20
+	 */
+	POTENTIALFRAUD(XfsDeviceState.POTENTIALFRAUD);
 
 	private final long value;
 
