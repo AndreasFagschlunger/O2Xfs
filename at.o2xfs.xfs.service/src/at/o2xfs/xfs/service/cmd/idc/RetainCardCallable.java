2d066650f362777f46d088658a3abcf90f9ee0d1
Andreas Fagschlunger <github@fagschlunger.co.at>
2012-07-21 19:18:38 +0200

    created

---
+++ at.o2xfs.xfs.service/src/at/o2xfs/xfs/service/cmd/idc/RetainCardCallable.java
@@ -0,0 +1,125 @@
+/*
+ * Copyright (c) 2012, Andreas Fagschlunger. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   - Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *   - Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package at.o2xfs.xfs.service.cmd.idc;
+
+import java.util.concurrent.Callable;
+
+import at.o2xfs.log.Logger;
+import at.o2xfs.log.LoggerFactory;
+import at.o2xfs.xfs.WFSResult;
+import at.o2xfs.xfs.XfsException;
+import at.o2xfs.xfs.idc.IDCExecuteCommand;
+import at.o2xfs.xfs.idc.WFSIDCRETAINCARD;
+import at.o2xfs.xfs.service.XfsServiceManager;
+import at.o2xfs.xfs.service.cmd.XfsCommand;
+import at.o2xfs.xfs.service.cmd.XfsExecuteCommand;
+import at.o2xfs.xfs.service.events.XfsEventNotification;
+import at.o2xfs.xfs.service.idc.IDCService;
+
+public class RetainCardCallable implements Callable<WFSIDCRETAINCARD>,
+		XfsEventNotification {
+
+	private static final Logger LOG = LoggerFactory
+			.getLogger(RetainCardCallable.class);
+
+	private XfsCommand retainCardCommand = null;
+
+	private WFSResult wfsResult = null;
+
+	public RetainCardCallable(final IDCService cardReader) {
+		if (cardReader == null) {
+			throw new IllegalArgumentException("cardReader must not be null");
+		}
+		retainCardCommand = new XfsExecuteCommand(cardReader,
+				IDCExecuteCommand.WFS_CMD_IDC_RETAIN_CARD);
+	}
+
+	@Override
+	public WFSIDCRETAINCARD call() throws InterruptedException, XfsException {
+		final String method = "call()";
+		synchronized (this) {
+			retainCardCommand.execute(this);
+			while (wfsResult == null) {
+				if (LOG.isDebugEnabled()) {
+					LOG.debug(method, "Waiting ...");
+				}
+				wait();
+			}
+		}
+		try {
+			XfsException.throwFor(wfsResult.getResult());
+			final WFSIDCRETAINCARD retainCard = new WFSIDCRETAINCARD(
+					wfsResult.getBuffer());
+			return new WFSIDCRETAINCARD(retainCard);
+		} finally {
+			XfsServiceManager.getInstance().free(wfsResult);
+			wfsResult = null;
+		}
+	}
+
+	public void cancel() throws XfsException {
+		retainCardCommand.cancel();
+	}
+
+	@Override
+	public void fireIntermediateEvent(final WFSResult wfsResult) {
+		try {
+			if (LOG.isInfoEnabled()) {
+				final String method = "fireIntermediateEvent(WFSResult)";
+				LOG.debug(method, "Intermediate event: " + wfsResult);
+			}
+		} finally {
+			if (wfsResult != null) {
+				XfsServiceManager.getInstance().free(wfsResult);
+			}
+		}
+	}
+
+	@Override
+	public void fireOperationCompleteEvent(final WFSResult wfsResult) {
+		final String method = "fireOperationCompleteEvent(WFSResult)";
+		if (LOG.isDebugEnabled()) {
+			LOG.debug(method, "wfsResult=" + wfsResult);
+		}
+		final IDCExecuteCommand commandCode = wfsResult
+				.getCommandCode(IDCExecuteCommand.class);
+		if (IDCExecuteCommand.WFS_CMD_IDC_RETAIN_CARD.equals(commandCode)) {
+			synchronized (this) {
+				this.wfsResult = wfsResult;
+				notifyAll();
+			}
+		} else {
+			if (LOG.isErrorEnabled()) {
+				LOG.error(method, "Unknown operation complete event: "
+						+ wfsResult);
+			}
+			XfsServiceManager.getInstance().free(wfsResult);
+		}
+	}
+
+}
