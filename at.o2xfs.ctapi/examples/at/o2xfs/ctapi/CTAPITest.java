4d442913f30d2daf35278d05d25bf6e7394307e0
Andreas Fagschlunger <github@fagschlunger.co.at>
2012-09-03 16:08:21 +0200

    CT-API Wrapper

---
+++ at.o2xfs.ctapi/examples/at/o2xfs/ctapi/CTAPITest.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2012, Andreas Fagschlunger. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   - Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *   - Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package at.o2xfs.ctapi;
+
+import at.o2xfs.common.Hex;
+
+public class CTAPITest {
+
+	private final CTAPI ctAPI = new CTAPI();
+
+	public void test() {
+		try {
+			final int pn = 1;
+			ctAPI.init(1, pn);
+			reset(1);
+			ctAPI.close(1);
+		} catch (final CTException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void reset(int ctn) throws CTException {
+		final byte[] command = new byte[] { 0x20, 0x11, 0x01, 0x01, 0x01, 0x01 };
+		final byte[] atr = ctAPI.data(ctn, 1, 2, command);
+		System.out.println("ATR: " + Hex.encode(atr));
+	}
+
+	public static final void main(final String[] args) {
+		new CTAPITest().test();
+	}
+}
---
+++ at.o2xfs.ctapi/native/CT-API/CT-API/ct_api.h
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2012, Andreas Fagschlunger. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   - Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *   - Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef ct_api_h
+#define ct_api_h
+#include <Windows.h>
+
+#define OK 0
+#define ERR_INVALID -1
+#define ERR_CT -8
+#define ERR_TRANS -10
+#define ERR_MEMORY -11
+#define ERR_HOST -127
+#define ERR_HTSI -128
+
+typedef CHAR (WINAPI *CT_INIT) (USHORT ctn, USHORT pn);
+typedef CHAR (WINAPI *CT_DATA) (USHORT ctn, UCHAR* dad, UCHAR* sad, USHORT lenc, UCHAR* command, USHORT* lenr, UCHAR* response);
+typedef CHAR (WINAPI *CT_CLOSE) (USHORT ctn);
+
+#endif
