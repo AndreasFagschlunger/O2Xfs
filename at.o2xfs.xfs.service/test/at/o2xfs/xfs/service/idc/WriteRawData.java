1e8f9dfa519b31a1b6fd0710479b8b1782966df3
AndreasFagschlunger <github@fagschlunger.co.at>
2014-03-09 13:57:25 +0100

    License Update

--- at.o2xfs.xfs.service/test/at/o2xfs/xfs/service/idc/WriteRawData.java
+++ at.o2xfs.xfs.service/test/at/o2xfs/xfs/service/idc/WriteRawData.java
@@ -1,3 +1,30 @@
+/*
+ * Copyright (c) 2014, Andreas Fagschlunger. All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 
+ *   - Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ * 
+ *   - Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
 package at.o2xfs.xfs.service.idc;
 
 import java.util.ArrayList;
@@ -28,11 +55,11 @@ public class WriteRawData extends XfsCommandTest {
 				.getServices(IDCService.class)) {
 			final List<WFSIDCCARDDATA> cardDataList = new ArrayList<WFSIDCCARDDATA>();
 			cardDataList.add(createCardData(idcService.getXfsVersion(),
-					IDCTrack.WFS_IDC_TRACK1, TRACK_1));
+					IDCTrack.TRACK1, TRACK_1));
 			cardDataList.add(createCardData(idcService.getXfsVersion(),
-					IDCTrack.WFS_IDC_TRACK2, TRACK_2));
+					IDCTrack.TRACK2, TRACK_2));
 			cardDataList.add(createCardData(idcService.getXfsVersion(),
-					IDCTrack.WFS_IDC_TRACK3, TRACK_3));
+					IDCTrack.TRACK3, TRACK_3));
 			System.out.println("cardDataList=" + cardDataList);
 			WFSResult wfsResult = null;
 			try {
--- at.o2xfs.xfs.service/test/at/o2xfs/xfs/service/pin/GetDataCommandTest.java
+++ at.o2xfs.xfs.service/test/at/o2xfs/xfs/service/pin/GetDataCommandTest.java
@@ -1,3 +1,30 @@
+/*
+ * Copyright (c) 2014, Andreas Fagschlunger. All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 
+ *   - Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ * 
+ *   - Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
 package at.o2xfs.xfs.service.pin;
 
 import java.util.EnumSet;
@@ -15,9 +42,9 @@ import at.o2xfs.xfs.pin.WFSPINFUNCKEYDETAIL;
 import at.o2xfs.xfs.pin.WfsPINGetData;
 import at.o2xfs.xfs.pin.WfsPINKey;
 import at.o2xfs.xfs.service.XfsCommandTest;
-import at.o2xfs.xfs.service.cmd.pin.PINDataListener;
-import at.o2xfs.xfs.service.cmd.pin.PINFunctionKeysCommand;
-import at.o2xfs.xfs.service.cmd.pin.PINGetDataCommand;
+import at.o2xfs.xfs.service.pin.cmd.PINDataListener;
+import at.o2xfs.xfs.service.pin.cmd.PINFunctionKeysCommand;
+import at.o2xfs.xfs.service.pin.cmd.PINGetDataCommand;
 
 public class GetDataCommandTest extends XfsCommandTest implements
 		PINDataListener {
@@ -48,7 +75,7 @@ public class GetDataCommandTest extends XfsCommandTest implements
 			throws InterruptedException, XfsException {
 		final String method = "createGetData(PINService)";
 		final WFSPINFUNCKEYDETAIL funcKeyDetail = new PINFunctionKeysCommand(
-				pinService).execute();
+				pinService).call();
 		if (LOG.isDebugEnabled()) {
 			LOG.debug(method, "funcKeyDetail=" + funcKeyDetail);
 		}
