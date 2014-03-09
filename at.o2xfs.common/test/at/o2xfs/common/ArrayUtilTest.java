13351f9e64f18fedbae4c005ec29e89394fb689a
AndreasFagschlunger <andreas.fagschlunger@reflex.at>
2014-03-09 13:31:29 +0100

    New Build, License Update

--- at.o2xfs.common/test/at/o2xfs/common/ArrayUtilTest.java
+++ at.o2xfs.common/test/at/o2xfs/common/ArrayUtilTest.java
@@ -27,62 +27,32 @@
 
 package at.o2xfs.common;
 
-public final class Bit {
+import org.junit.Assert;
+import org.junit.Test;
 
-	/**
-	 * Bitmask for the first bit
-	 */
-	public static final int B1 = 1;
+public class ArrayUtilTest {
 
-	/**
-	 * Bitmask for the second bit
-	 */
-	public static final int B2 = 2;
-
-	/**
-	 * Bitmask for the third bit
-	 */
-	public static final int B3 = 4;
-
-	/**
-	 * Bitmask for the forth bit
-	 */
-	public static final int B4 = 8;
-
-	/**
-	 * Bitmask for the fifth bit
-	 */
-	public static final int B5 = 16;
-
-	/**
-	 * Bitmask for the sixth bit
-	 */
-	public static final int B6 = 32;
-
-	/**
-	 * Bitmask for the seventh bit
-	 */
-	public static final int B7 = 64;
-
-	/**
-	 * Bitmask for the eighth bit
-	 */
-	public static final int B8 = 128;
-
-	private Bit() {
-		throw new AssertionError();
+	@Test
+	public final void noElements() {
+		byte[] expecteds = new byte[] {};
+		byte[] actuals = new byte[] {};
+		ArrayUtil.reverse(actuals);
+		Assert.assertArrayEquals(expecteds, actuals);
 	}
 
-	/**
-	 * Checks if the specified byte matches the specified mask.
-	 * 
-	 * @param b
-	 *            the <code>byte</code> to check
-	 * @param mask
-	 *            the mask to which <code>b</code> is matched
-	 * @return true if <code>b</code> matches the given mask
-	 */
-	public static final boolean isSet(byte b, int mask) {
-		return (b & mask) == mask;
+	@Test
+	public final void evenNumberOfElements() {
+		byte[] expecteds = new byte[] { 4, 3, 2, 1 };
+		byte[] actuals = new byte[] { 1, 2, 3, 4 };
+		ArrayUtil.reverse(actuals);
+		Assert.assertArrayEquals(expecteds, actuals);
+	}
+
+	@Test
+	public final void oddNumberOfElements() {
+		byte[] expecteds = new byte[] { 3, 2, 1 };
+		byte[] actuals = new byte[] { 1, 2, 3 };
+		ArrayUtil.reverse(actuals);
+		Assert.assertArrayEquals(expecteds, actuals);
 	}
 }
