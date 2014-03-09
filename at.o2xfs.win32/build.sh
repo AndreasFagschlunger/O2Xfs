3340f83b8d1a7385315ed19dbbe6a36a00cecb96
AndreasFagschlunger <github@fagschlunger.co.at>
2014-03-09 13:38:57 +0100

    New Build

--- at.o2xfs.win32/build.sh
+++ at.o2xfs.win32/build.sh
@@ -1,1 +1,14 @@
-make -f "`dirname "$0"`/native/O2XfsWIN32/O2XfsWIN32.mk" basedir="`dirname "$0"`"
+#!bin/sh
+
+cygwin=false
+
+if [ "$(uname -o)" = "Cygwin" ]
+then
+    cygwin=true
+fi
+
+function fixpath () {
+	echo $1 | sed -e "s/\([A-z]\):/cygdrive\/\1/"
+}
+
+make -f "$(dirname $0)/make.mk" JAVA_HOME="$1" SRC_DIR="$(dirname $0)/native/O2XfsWIN32" BIN_DIR="$3"
