57c610280841b5db4e32366c975b3a8667c05425
Andreas Fagschlunger <andreas@fagschlunger.co.at>
2015-04-01 15:04:44 +0200

    CT-API: Allow differen library name

--- at.o2xfs.ctapi/src/at/o2xfs/ctapi/CTAPI.java
+++ at.o2xfs.ctapi/src/at/o2xfs/ctapi/CTAPI.java
@@ -5,17 +5,17 @@
  * modification, are permitted provided that the following conditions
  * are met:
  * 
- *   - Redistributions of source code must retain the above copyright
- *     notice, this list of conditions and the following disclaimer.
+ * - Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
  * 
- *   - Redistributions in binary form must reproduce the above copyright
- *     notice, this list of conditions and the following disclaimer in the
- *     documentation and/or other materials provided with the distribution.
+ * - Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
  * 
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
  * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
  * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
  * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
  * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
  * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
@@ -45,7 +45,7 @@ public final class CTAPI {
 
 	private static final Logger LOG = LoggerFactory.getLogger(CTAPI.class);
 
-	private static final String DEFAULT_LIBRARY = "CT32.dll";
+	private String fileName = "CT32.dll";
 
 	private Pointer moduleHandle = null;
 
@@ -56,11 +56,7 @@ public final class CTAPI {
 	private Pointer closeFunctionAddress = null;
 
 	public CTAPI() {
-		this(DEFAULT_LIBRARY);
-	}
-
-	public CTAPI(String fileName) {
-		loadLibrary(fileName);
+		loadLibrary("C:\\Program Files (x86)\\Identive\\SPR532 Driver\\CT-API Tools\\CTPCSC32.dll");
 		addShutdownHook();
 	}
 
@@ -78,21 +74,20 @@ public final class CTAPI {
 		}
 	}
 
-	private native Buffer loadLibrary0(final ZSTR fileName)
-			throws NativeException;
+	private native Buffer loadLibrary0(final ZSTR fileName) throws NativeException;
 
 	private Buffer getFunctionAddress(final String name) {
 		try {
 			return getFunctionAddress0(moduleHandle, new ZSTR(name));
 		} catch (final NativeException e) {
-			throw new RuntimeException(
-					"Error getting function address: moduleHandle="
-							+ moduleHandle + ",name=" + name, e);
+			throw new RuntimeException(	"Error getting function address: moduleHandle=" + moduleHandle
+												+ ",name="
+												+ name,
+										e);
 		}
 	}
 
-	private native Buffer getFunctionAddress0(final Pointer moduleHandle,
-			final ZSTR name) throws NativeException;
+	private native Buffer getFunctionAddress0(final Pointer moduleHandle, final ZSTR name) throws NativeException;
 
 	/**
 	 * 
@@ -103,28 +98,30 @@ public final class CTAPI {
 	 *            Port number of the physical interface
 	 */
 	public void init(int ctn, int pn) throws CTException {
-		final int rc = init0(initFunctionAddress, new USHORT(ctn), new USHORT(
-				pn));
+		final int rc = init0(initFunctionAddress, new USHORT(ctn), new USHORT(pn));
 		CTException.throwFor(rc);
 	}
 
 	private native int init0(Pointer address, USHORT ctn, USHORT pn);
 
-	public byte[] data(int ctn, int dad, int sad, byte[] command)
-			throws CTException {
+	public byte[] data(int ctn, int dad, int sad, byte[] command) throws CTException {
 		final ByteArray cmdBuf = new ByteArray(command);
 		final ByteArray rspBuf = new ByteArray(258);
 		final USHORT lenr = new USHORT(rspBuf.getSize());
-		final int rc = data0(dataFunctionAddress, new USHORT(ctn), new UINT8(
-				dad), new UINT8(sad), cmdBuf, lenr, rspBuf);
+		final int rc = data0(dataFunctionAddress, new USHORT(ctn), new UINT8(dad), new UINT8(sad), cmdBuf, lenr, rspBuf);
 		CTException.throwFor(rc);
 		final byte[] rsp = new byte[lenr.intValue()];
 		System.arraycopy(rspBuf.toByteArray(), 0, rsp, 0, rsp.length);
 		return rsp;
 	}
 
-	private native int data0(Pointer address, USHORT ctn, UINT8 dad, UINT8 sad,
-			ByteArray command, USHORT lenr, ByteArray response);
+	private native int data0(	Pointer address,
+								USHORT ctn,
+								UINT8 dad,
+								UINT8 sad,
+								ByteArray command,
+								USHORT lenr,
+								ByteArray response);
 
 	public void close(int ctn) throws CTException {
 		final int rc = close0(closeFunctionAddress, new USHORT(ctn));
@@ -144,9 +141,7 @@ public final class CTAPI {
 						moduleHandle = null;
 					} catch (final NativeException e) {
 						final String method = "run()";
-						LOG.error(method,
-								"Error freeing library: moduleHandle="
-										+ moduleHandle, e);
+						LOG.error(method, "Error freeing library: moduleHandle=" + moduleHandle, e);
 					}
 				}
 			}
@@ -154,6 +149,5 @@ public final class CTAPI {
 		});
 	}
 
-	private native void freeLibrary0(final Pointer moduleHandle)
-			throws NativeException;
+	private native void freeLibrary0(final Pointer moduleHandle) throws NativeException;
 }
