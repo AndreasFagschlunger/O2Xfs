b84760035ba93fb895f763af13148b78354d8706
Andreas Fagschlunger <andreas@fagschlunger.co.at>
2015-04-15 10:41:51 +0200

    Changed from absolute java.exe path to simply java

--- at.o2xfs.operator/start.cmd
+++ at.o2xfs.operator/start.cmd
@@ -3,5 +3,5 @@ CLS
 SET workingDirectory=%~dp0
 SET workingDirectory=%workingDirectory:~0,-1%
 IF NOT EXIST %workingDirectory%\%COMPUTERNAME% MD %workingDirectory%\%COMPUTERNAME%
-"C:\Program Files (x86)\Java\jre7\bin\java.exe" -Dlog4j.configuration=file:/%workingDirectory%\config\log4j.xml -Djava.library.path=%workingDirectory%\bin -jar %workingDirectory%\lib\at.o2xfs.operator.jar > %workingDirectory%\%COMPUTERNAME%\out.log 2> %workingDirectory%\%COMPUTERNAME%\err.log
+java -Dlog4j.configuration=file:/%workingDirectory%\config\log4j.xml -Djava.library.path=%workingDirectory%\bin -jar %workingDirectory%\lib\at.o2xfs.operator.jar > %workingDirectory%\%COMPUTERNAME%\out.log 2> %workingDirectory%\%COMPUTERNAME%\err.log
 MOVE /Y *.log* %workingDirectory%\%COMPUTERNAME%
