2a89fbd6d0de10b4802448643d6fd26907b12237
AndreasFagschlunger <github@fagschlunger.co.at>
2016-06-12 23:52:22 +0200

    XfsServiceClass.java: Added CIM

--- at.o2xfs.xfs/src/main/java/at/o2xfs/xfs/XfsServiceClass.java
+++ at.o2xfs.xfs/src/main/java/at/o2xfs/xfs/XfsServiceClass.java
@@ -1,17 +1,17 @@
 /*
  * Copyright (c) 2014, Andreas Fagschlunger. All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
  * are met:
- * 
+ *
  * - Redistributions of source code must retain the above copyright
  * notice, this list of conditions and the following disclaimer.
- * 
+ *
  * - Redistributions in binary form must reproduce the above copyright
  * notice, this list of conditions and the following disclaimer in the
  * documentation and/or other materials provided with the distribution.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
  * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
  * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
@@ -77,7 +77,12 @@ public enum XfsServiceClass implements XfsConstant {
 	/**
 	 * Cameras
 	 */
-	CAM(10L, "CAM");
+	CAM(10L, "CAM"),
+
+	/**
+	 * Cash-In Module
+	 */
+	CIM(13L, "CIM");
 
 	/**
 	 * Class Identifier
