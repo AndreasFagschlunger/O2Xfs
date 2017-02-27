9041c7e6384fe83829982c5d6c10fbde9aeb6687
AndreasFagschlunger <github@fagschlunger.co.at>
2017-02-27 23:23:33 +0100

    xfs-sdk.ps1: Improved parameter handling

--- xfs-sdk.ps1
+++ xfs-sdk.ps1
@@ -1,30 +1,35 @@
-$path = 'C:\XFS SDKs'
-$zipExe = '7z.exe'
+Param(
+  [string]$sevenZipPath = '',
+  [string]$sdkPath = 'C:\XFS SDKs'
+)
 
-if($args.count -gt 0) {
-  $path = $args[0]
+if($sevenZipPath.length -gt 0 -and -not $sevenZipPath.EndsWith('\')) {
+  $sevenZipPath = $sevenZipPath + '\'
 }
-if($args.count -gt 1) {
-  $zipExe = $args[1] + '\' + $zipExe
+
+if($sdkPath.EndsWith('\')) {
+  $sdkPath = $sdkPath.Substring(0, $sdkPath.length - 1)
 }
+
+$zipExe = $sevenZipPath+ '7z.exe'
 
 [Net.ServicePointManager]::SecurityProtocol = 'Tls12'
 
-New-Item -ItemType 'directory' -Force -Path $path
-Invoke-WebRequest 'https://repo.fagschlunger.co.at/install/eu/cencenelec/XFS/sdk303.zip' -OutFile "$path\sdk303.zip"
-Invoke-WebRequest 'https://repo.fagschlunger.co.at/install/eu/cencenelec/XFS/XFS310SDKInstall.zip' -OutFile "$path\XFS310SDKInstall.zip"
-Invoke-WebRequest 'https://repo.fagschlunger.co.at/install/eu/cencenelec/XFS/SDK320.zip' -OutFile "$path\SDK320.zip"
-Invoke-WebRequest 'https://repo.fagschlunger.co.at/install/eu/cencenelec/XFS/SDK330.zip' -OutFile "$path\SDK330.zip"
+New-Item -ItemType 'directory' -Force -Path $sdkPath
+Invoke-WebRequest 'https://repo.fagschlunger.co.at/install/eu/cencenelec/XFS/sdk303.zip' -OutFile "$sdkPath\sdk303.zip"
+Invoke-WebRequest 'https://repo.fagschlunger.co.at/install/eu/cencenelec/XFS/XFS310SDKInstall.zip' -OutFile "$sdkPath\XFS310SDKInstall.zip"
+Invoke-WebRequest 'https://repo.fagschlunger.co.at/install/eu/cencenelec/XFS/SDK320.zip' -OutFile "$sdkPath\SDK320.zip"
+Invoke-WebRequest 'https://repo.fagschlunger.co.at/install/eu/cencenelec/XFS/SDK330.zip' -OutFile "$sdkPath\SDK330.zip"
 
-& "$zipExe" e -o"$path" -r "$path\sdk303.zip" Install.EXE
-& "$zipExe" e -o"$path" -r "$path\Install.EXE" XFS_Manager_SDK.msi Data1.cab
-& msiexec /a "$path\XFS_Manager_SDK.msi" /qb TARGETDIR='"'$path'\SDK303"'
+& "$zipExe" e -o"$sdkPath" -r "$sdkPath\sdk303.zip" Install.EXE
+& "$zipExe" e -o"$sdkPath" -r "$sdkPath\Install.EXE" XFS_Manager_SDK.msi Data1.cab
+& msiexec /a "$sdkPath\XFS_Manager_SDK.msi" /qb TARGETDIR='"'$sdkPath'\SDK303"'
 
-& "$zipExe" e -o"$path" -r "$path\XFS310SDKInstall.zip" XFS310SDKInstall.exe
-& "$path\XFS310SDKInstall.exe" /a /s /v'/qn TARGETDIR=\"'$path'\SDK310\"'
+& "$zipExe" e -o"$sdkPath" -r "$sdkPath\XFS310SDKInstall.zip" XFS310SDKInstall.exe
+& "$sdkPath\XFS310SDKInstall.exe" /a /s /v'/qn TARGETDIR=\"'$sdkPath'\SDK310\"'
 
-& "$zipExe" e -o"$path" -r "$path\SDK320.zip"
-& msiexec /a "$path\SDK320.msi" /qn TARGETDIR='"'$path'\SDK320"'
+& "$zipExe" e -o"$sdkPath" -r "$sdkPath\SDK320.zip"
+& msiexec /a "$sdkPath\SDK320.msi" /qn TARGETDIR='"'$sdkPath'\SDK320"'
 
-& "$zipExe" e -o"$path" -r "$path\SDK330.zip"
-& msiexec /a "$path\SDK330.msi" /qn TARGETDIR='"'$path'\SDK330"'
+& "$zipExe" e -o"$sdkPath" -r "$sdkPath\SDK330.zip"
+& msiexec /a "$sdkPath\SDK330.msi" /qn TARGETDIR='"'$sdkPath'\SDK330"'
