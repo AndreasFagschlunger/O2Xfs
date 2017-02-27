14bad7d27ea842967a89fc9b9f96df9a7368977c
AndreasFagschlunger <github@fagschlunger.co.at>
2017-02-27 01:28:59 +0100

    AppVeyor: build errors

--- xfs-sdk.ps1
+++ xfs-sdk.ps1
@@ -19,18 +19,12 @@ Invoke-WebRequest 'https://repo.fagschlunger.co.at/install/eu/cencenelec/XFS/SDK
 & "$zipExe" e -o"$path" -r "$path\sdk303.zip" Install.EXE
 & "$zipExe" e -o"$path" -r "$path\Install.EXE" XFS_Manager_SDK.msi Data1.cab
 & msiexec /a "$path\XFS_Manager_SDK.msi" /qb TARGETDIR='"'$path'\SDK303"'
-Remove-Item "$path\Install.EXE"
-Remove-Item "$path\XFS_Manager_SDK.msi"
-Remove-Item "$path\Data1.cab"
 
 & "$zipExe" e -o"$path" -r '$path\XFS310SDKInstall.zip' XFS310SDKInstall.exe
 & "$path\XFS310SDKInstall.exe" /a /s /v'/qn TARGETDIR=\"'$path'\SDK310\"'
-Remove-Item "$path\XFS310SDKInstall.exe"
 
 & "$zipExe" e -o"$path" -r "$path\SDK320.zip"
 & msiexec /a "$path\SDK320.msi" /qn TARGETDIR='"'$path'\SDK320"'
-Remove-Item "$path\SDK320.msi"
 
 & "$zipExe" e -o"$path" -r "$path\SDK330.zip"
 & msiexec /a "$path\SDK330.msi" /qn TARGETDIR='"'$path'\SDK330"'
-Remove-Item "$path\SDK330.msi"
