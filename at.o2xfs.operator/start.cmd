590aa24821483e9078af66d91c59448bde06fc0f
AndreasFagschlunger <github@fagschlunger.co.at>
2017-03-05 21:54:20 +0100

    at.o2xfs.operator: start.cmd: added version to JAR name

--- at.o2xfs.operator/start.cmd
+++ at.o2xfs.operator/start.cmd
@@ -2,5 +2,5 @@
 CLS
 SET workingDirectory=%~dp0
 SET workingDirectory=%workingDirectory:~0,-1%
-IF NOT EXIST %workingDirectory%\%COMPUTERNAME% MD %workingDirectory%\%COMPUTERNAME%
-java -Dlog4j.configuration=file:/%workingDirectory%\config\log4j.xml -jar %workingDirectory%\lib\at.o2xfs.operator.jar > %workingDirectory%\%COMPUTERNAME%\out.log 2> %workingDirectory%\%COMPUTERNAME%\err.log
+IF NOT EXIST %workingDirectory%\\%COMPUTERNAME% MD %workingDirectory%\\%COMPUTERNAME%
+java -Dlog4j.configuration=file:/%workingDirectory%\\config\\log4j.xml -jar %workingDirectory%\\lib\\at.o2xfs.operator-${version}.jar > %workingDirectory%\\%COMPUTERNAME%\\out.log 2> %workingDirectory%\\%COMPUTERNAME%\\err.log
